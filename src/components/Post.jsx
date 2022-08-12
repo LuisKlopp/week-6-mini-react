de0ad2de3347f604f8bfd8353ffb0bd64867093d
LuisKlopp <fbgus333@gmail.com>
2022-08-12 16:36:58 +0900

    컴포넌트 구조잡기

---
+++ src/components/Post.jsx
@@ -0,0 +1,37 @@
+/*eslint-disable*/
+import React from "react";
+import styled, { createGlobalStyle } from "styled-components";
+
+
+const Post = () => {
+
+
+  return (
+    <>
+      <GlobalStyle/>
+      <StWrapper>
+        Post Page
+      </StWrapper>
+    </>
+  );
+};
+
+export default Post;
+
+
+const GlobalStyle = createGlobalStyle`
+	body {
+		padding: 0;
+		margin: 0;
+    background-color: ecdede;
+    
+	}
+  `;
+
+const StWrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
